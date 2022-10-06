2e0a884ef43fb07e6bf319c014b4ba7a4df92609
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2022-10-06 11:00:46 +0700

    Upp

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -30,7 +30,7 @@ function ProjectCards(props) {
             style={{ marginLeft: "10px" }}
           >
             <CgWebsite /> &nbsp;
-            {"Demo"}
+            {"Klik Saya"}
           </Button>
         )}
       </Card.Body>
