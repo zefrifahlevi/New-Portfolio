25b54c70043081e7b711f4e344ffce415c487054
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2022-10-08 11:08:49 +0700

    Up

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/zefrifahlevi/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/zefrifahlevi/new-portfolio/master/src/Assets/CV_Zefri_Fahlevi.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
